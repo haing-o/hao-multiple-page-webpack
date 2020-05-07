c58b03e40132da8a3bc328f521e55f7b5580c2fb
guangtt <bbb3520@163.com>
2020-05-07 16:43:15 +0800

    区分production/development前

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import style from'./index.scss'
-import avatar from './avatar.png'
-import createAvatar from './creatAvatar.js'
-
-createAvatar();
-
-let img = new Image();
-img.src = avatar;
-img.classList.add(style.avatar);
-let root = document.querySelector('#root');
-root.append(img);
+const arr = [
+  new Promise(() => {}),
+  new Promise(() => {})
+]
+arr.map(item => {
+  console.log(item)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development', // 不配置会警告
+  // cheap只精确到行 inline将map文件内嵌在js里 eval更快也内嵌在js里 不新建一个map module还检查第三方loader或者plugin里的错误
+  devtool: 'cheap-module-eval-source-map', // development
+  // devtool: 'cheap-module-source-map', // production
   // 默认的chunk name就是main
   entry: {
     'main': './src/index.js',
-    'sub': './src/index.js'
+  },
+  devServer: {
+    contentBase: './dist', // 服务器基于什么文件夹运行
+    // open: true, // 自动打开浏览器
+    port: 8090,
+    hot: true, //热更新
+    hotOnly: true //禁止浏览器自动刷新
   },
   module: {
     rules: [
@@ -43,6 +53,32 @@ module.exports = {
           },
           'sass-loader',
           'postcss-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2, // 文件内@import引入的另一个样式文件，也要先通过css-loader前的2个loader编译
+              modules: true // 模块化
+            }
+          },
+          'postcss-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", {
+            targets: {
+              chrome: "67"
+            },
+            useBuiltIns: 'usage'
+          }]]
+        }
       }
     ]
   },
@@ -53,11 +89,18 @@ module.exports = {
       template: 'src/index.html'
     }),
     // 默认清空dist文件夹
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
+  // Tree Shaking 只对ES Module起作用
+  // package.json中的sideEffects可以添加忽略项(不管是否引用都打包)
+  // 测试环境下不会真正删除代码，只会提示 正式环境下才会彻底删除没用到的代码 减少代码量
+  optimization: {
+    usedExports: true
+  },
   output: {
     // publicPath: 'http://cdn.com', // 用于在js文件前加公用前缀
-    filename: '[name].[hash].js', //[name]代表直接使用入口命名
+    filename: '[name].js', //[name]代表直接使用入口命名
     path: path.resolve(__dirname, 'dist') // 默认就是dist文件夹
   }
 }
